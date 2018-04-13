99a45cc2947c0fbd2978e72f67bf50f3d0f4a835
Enrico Vettori <enrico.vettori93@gmail.com>
2018-04-13 16:17:22 +0200

    Aggiunta funzionalità di delete data

--- Src/unfinitaly/app/src/main/java/it/unive/dais/bunnyteam/unfinitaly/app/view/CustomDialogPreference.java
+++ Src/unfinitaly/app/src/main/java/it/unive/dais/bunnyteam/unfinitaly/app/view/CustomDialogPreference.java
@@ -1,18 +1,26 @@
 package it.unive.dais.bunnyteam.unfinitaly.app.view;
 
+import android.app.ActivityManager;
+import android.content.DialogInterface;
 import android.preference.DialogPreference;
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.os.Parcel;
-import android.os.Parcelable;
-import android.preference.DialogPreference;
 import android.util.AttributeSet;
-import android.preference.DialogPreference;
 
-public class CustomDialogPreference extends DialogPreference
+public class CustomDialogPreference extends DialogPreference implements DialogInterface.OnClickListener
 {
+    Context context;
     public CustomDialogPreference(Context oContext, AttributeSet attrs)
     {
         super(oContext, attrs);
+        context = oContext;
+        setPositiveButtonText("Conferma");
+        setNegativeButtonText("Annulla");
+    }
+    @Override
+    public void onClick(DialogInterface dialog, int which){
+        if(which == DialogInterface.BUTTON_POSITIVE) {
+            //Cancello i dati dell'applicazione
+            ((ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE)).clearApplicationUserData();
+        }
     }
 }
